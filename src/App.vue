2b4564dbf2fbf9ecb1b8b253ed68de881ad62150
David Balboa <david@devils.local>
2020-02-07 00:06:10 +0100

    vue is cool

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container">
+            <router-link class="navbar-brand" to="/">wanna watch?</router-link>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+              <ul class="navbar-nav">
+                <li class="nav-item active">
+                  <router-link class="nav-link" to="/movies">Movies</router-link>
+                </li>
+                <li class="nav-item">
+                  <router-link class="nav-link" to="/tv">Tv-Shows</router-link>
+                </li>
+              </ul>
+            </div>
+            <form class="form-inline">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+              </form>
+            </div>
+        </nav>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
---
+++ src/components/search.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>{{ msg }}</h1>
+        <p>
+            holaaaaaaaa
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'search',
+  data () {
+    return {
+      msg: 'im a noob :('
+    }
+  }
+}
+</script>
